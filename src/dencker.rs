b248f6473fe6664111fe0744277d4637c20fbc0a
Brooks McKinley <brooksmckinley@yahoo.com>
2019-11-20 12:46:39 -0500

    Auto-format code

--- src/dencker.rs
+++ src/dencker.rs
@@ -1,61 +1,60 @@
-use Food::*;
 use rand::prelude::*;
+use Food::*;
 
 pub struct Dencker {
-	max_tolerance: i32,
-	current_points: i32,
-	current_score: i32,
+    max_tolerance: i32,
+    current_points: i32,
+    current_score: i32,
 }
 
 impl Dencker {
-	pub fn new() -> Dencker {
-		let mut rng = thread_rng();
-		Dencker {
-			max_tolerance: rng.gen_range(25, 75),
-			current_points: 0,
-			current_score: 0,
-		}
-	}
-	pub fn feed(&mut self, food: Food) -> bool {
-		let mut rng = thread_rng();
-		match food {
-			Turkey => {
-				self.current_points += rng.gen_range(0, 10);
-				self.current_score += 3;
-			},
-			MashedPotatos => {
-				self.current_points += rng.gen_range(0, 5);
-				self.current_score += 1;
-			},
-			GreenBeans => {
-				self.current_points += rng.gen_range(2, 15);
-				self.current_score += 5;
-			},
-			Casserole => {
-				self.current_points += rng.gen_range(10, 20);
-				self.current_score += 15;
-			},
-			Mix => {
-				self.current_points += rng.gen_range(10, 25);
-				self.current_score += 30;
-			}
-		};
-		if self.current_points > self.max_tolerance {
-			true
-		}
-		else {
-			false
-		}
-	}
-	pub fn get_score(&self) -> i32 { 
-		self.current_score 
-	}
+    pub fn new() -> Dencker {
+        let mut rng = thread_rng();
+        Dencker {
+            max_tolerance: rng.gen_range(25, 75),
+            current_points: 0,
+            current_score: 0,
+        }
+    }
+    pub fn feed(&mut self, food: Food) -> bool {
+        let mut rng = thread_rng();
+        match food {
+            Turkey => {
+                self.current_points += rng.gen_range(0, 10);
+                self.current_score += 3;
+            }
+            MashedPotatos => {
+                self.current_points += rng.gen_range(0, 5);
+                self.current_score += 1;
+            }
+            GreenBeans => {
+                self.current_points += rng.gen_range(2, 15);
+                self.current_score += 5;
+            }
+            Casserole => {
+                self.current_points += rng.gen_range(10, 20);
+                self.current_score += 15;
+            }
+            Mix => {
+                self.current_points += rng.gen_range(10, 25);
+                self.current_score += 30;
+            }
+        };
+        if self.current_points > self.max_tolerance {
+            true
+        } else {
+            false
+        }
+    }
+    pub fn get_score(&self) -> i32 {
+        self.current_score
+    }
 }
 
 pub enum Food {
-	Turkey,
-	MashedPotatos,
-	GreenBeans,
-	Casserole,
-	Mix,
+    Turkey,
+    MashedPotatos,
+    GreenBeans,
+    Casserole,
+    Mix,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 // Classroom said this assignment was 100 points, so I'm going all out.
 // Have fun
 
-extern crate rand;
 extern crate cursive;
+extern crate rand;
 
 mod dencker;
 mod interface;
@@ -10,8 +10,8 @@ mod interface;
 use cursive::Cursive;
 
 fn main() {
-	let mut siv = Cursive::ncurses().unwrap();
-	interface::start_game(&mut siv);
-	siv.run();
-	println!("Have a great day!");
+    let mut siv = Cursive::ncurses().unwrap();
+    interface::start_game(&mut siv);
+    siv.run();
+    println!("Have a great day!");
 }
