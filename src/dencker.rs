a70a6ba338d51f16c81ec68b8f118986d7223afa
Brooks McKinley <brooksmckinley@yahoo.com>
2019-11-20 11:24:02 -0500

    Implemented Dencker-related functions

--- src/dencker.rs
+++ src/dencker.rs
@@ -1,14 +1,69 @@
+use Food::*;
+use rand::prelude::*;
+
 pub struct Dencker {
-	max_tolerange: i32,
+	max_tolerance: i32,
 	current_points: i32,
 }
 
 impl Dencker {
+	pub fn new() -> Dencker {
+		let mut rng = thread_rng();
+		Dencker {
+			max_tolerance: rng.gen_range(25, 75),
+			current_points: 0,
+		}
+	}
 	pub fn feed(&mut self, food: Food) -> bool {
-		unimplemented!()
+		let mut rng = thread_rng();
+		match food {
+			Turkey => {
+				let add = rng.gen_range(0, 10);
+				self.current_points += add;
+			},
+			MashedPotatos => {
+				let add = rng.gen_range(0, 5);
+				self.current_points += add;
+			},
+			GreenBeans => {
+				let add = rng.gen_range(2, 15);
+				self.current_points += add;
+			},
+			Casserole => {
+				let add = rng.gen_range(10, 20);
+				self.current_points += add;
+			},
+		};
+		if self.current_points > self.max_tolerance {
+			true
+		}
+		else {
+			false
+		}
 	}
 	pub fn mix(&mut self, foods: &[Food]) -> bool {
-		unimplemented!()
+		for food in foods {
+			match food {
+				Turkey => {
+					self.current_points += 3;
+				},
+				MashedPotatos => {
+					self.current_points += 1;
+				},
+				GreenBeans => {
+					self.current_points += 5;
+				},
+				Casserole => {
+					self.current_points += 15;
+				},
+			}
+		}
+		if self.current_points > self.max_tolerance {
+			true
+		}
+		else {
+			false
+		}
 	}
 }
 
