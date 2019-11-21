715baf3118d81ddf21453747266a7779a23495ec
Brooks McKinley <brooksmckinley@yahoo.com>
2019-11-21 08:35:00 -0500

    Added theme

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ extern crate rand;
 
 mod dencker;
 mod interface;
+mod theme;
 
 use cursive::Cursive;
 
 fn main() {
     let mut siv = Cursive::ncurses().unwrap();
+    siv.set_theme(theme::get_theme());
     interface::start_game(&mut siv);
     siv.run();
     println!("Have a great day!");
